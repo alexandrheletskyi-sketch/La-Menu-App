4744752cde02bf5c558e6725929d50e9a6062b69
alexandrheletskyi-sketch <alexandrheletskyi@gmail.com>
2026-03-25 17:21:13 +0100

    Initial Commit

---
+++ La Menu/La_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  La_MenuApp.swift
+//  La Menu
+//
+//  Created by MacBook Pro on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct La_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
